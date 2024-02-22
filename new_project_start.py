c8d83ea2358fcb972c989822e3f1d2ad4b906c67
outsider_analytics <brian@outsideranalytics.xyz>
2024-02-22 13:57:23 -0700

    set up ready

--- new_project_start.py
+++ new_project_start.py
@@ -3,6 +3,14 @@ import re
 import os
 
 
+def update_queries_yml():
+    # Path to queries.yml in the root directory
+    yml_path = os.path.join(os.getcwd(), "queries.yml")
+    with open(yml_path, "w") as file:
+        file.write("query_ids:\n")
+    print("queries.yml has been reset.")
+
+
 def main():
     print("🎉 Setting up a new project for a Steakhouse Financial Dune Dashboard! 🎉")
     while True:
@@ -14,9 +22,18 @@ def main():
         else:
             print("Invalid project name. Make sure it meets the criteria.")
 
+    # Replace queries.yml content
+    update_queries_yml()
+
     # Assuming change_project_name.py is in the 'scripts' folder and callable
-    script_path = os.path.join("scripts", "change_project_name.py")
-    subprocess.run(["python", script_path, new_prefix], check=True)
+    change_script_path = os.path.join("scripts", "change_project_name.py")
+    subprocess.run(["python", change_script_path, new_prefix], check=True)
+
+    # After renaming files, run push_to_dune.py to push changes
+    push_script_path = os.path.join("scripts", "push_to_dune.py")
+    subprocess.run(["python", push_script_path], check=True)
+
+    print("Your template has been added to Dune successfully!")
 
 
 if __name__ == "__main__":
